494c3118f6ef5cec61d51714f5760c63fd6ff4a6
anton <antonsebrianto525@gmail.com>
2021-11-23 01:09:29 +0700

    Retrovolley

---
+++ Retrovolley/app/src/main/java/com/anton/retrovolley/retrofit/MethodHTTP.java
@@ -0,0 +1,19 @@
+package com.anton.retrovolley.retrofit;
+
+import com.anton.retrovolley.Request;
+import com.anton.retrovolley.User;
+import com.anton.retrovolley.UserResponse;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface MethodHTTP {
+
+    @GET("User_Registration.php")
+    Call<UserResponse> getUser();
+
+    @POST("User_Registration.php")
+    Call<Request> sendUser(@Body User user);
+}
